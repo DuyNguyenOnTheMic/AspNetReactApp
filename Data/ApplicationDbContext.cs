fb95dc5a87fd971d1d1dd07df75ff02c91d1529f
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-07-01 23:39:59 +0700

    Update packages

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -8,10 +8,8 @@ namespace AspNetReactApp.Data
 {
     public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
     {
-        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
-            : base(options, operationalStoreOptions)
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
         {
-
         }
 
         public DbSet<Student> Students { get; set; }
