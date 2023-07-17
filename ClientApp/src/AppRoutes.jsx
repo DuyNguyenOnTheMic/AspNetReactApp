f1ceb8d41fafd868f29b14869e087f9c522149ff
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-07-17 11:05:19 +0700

    Update add react types to project

--- ClientApp/src/AppRoutes.jsx
+++ ClientApp/src/AppRoutes.jsx
@@ -1,5 +1,6 @@
 import { Counter } from './components/Counter'
 import { Home } from './components/Home'
+import StudentManagement from './components/StudentManagement'
 import { WeatherForecast } from './components/WeatherForecast'
 import ApiAuthorzationRoutes from './components/api-authorization/ApiAuthorizationRoutes'
 
@@ -16,6 +17,11 @@ const AppRoutes = [
     path: '/weather-forecast',
     requireAuth: true,
     element: <WeatherForecast />
+  },
+  {
+    path: '/student-management',
+    requireAuth: true,
+    element: <StudentManagement />
   },
   ...ApiAuthorzationRoutes
 ]
