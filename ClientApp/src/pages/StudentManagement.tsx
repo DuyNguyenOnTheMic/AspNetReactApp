0f44f8af720683864f3ac0507790ab8b7ea14524
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-07-23 21:25:08 +0700

    Update move modal outside of component class

--- ClientApp/src/pages/StudentManagement.tsx
+++ ClientApp/src/pages/StudentManagement.tsx
@@ -1,11 +1,36 @@
-import { Fragment, useEffect, useState } from 'react'
+import { FormEvent, Fragment, Reducer, useEffect, useReducer, useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Table from 'react-bootstrap/Table'
-import { fetchData } from 'src/api/students'
+import FloatingLabel from 'react-bootstrap/esm/FloatingLabel'
+import Form from 'react-bootstrap/esm/Form'
+import Modal from 'react-bootstrap/esm/Modal'
+import { addStudent, fetchData } from 'src/api/students'
 import { StudentsType } from 'src/types/studentTypes'
 import CustomPagination from './components/CustomPagination'
-import ModalStudentForm from './components/ModalStudentForm'
 import useSortableData from './components/useSortableData'
+
+interface IAction {
+  type: string
+  value?: string
+}
+
+const initialState: StudentsType = {
+  id: '',
+  name: '',
+  age: Number(''),
+  course: '',
+  note: ''
+}
+
+function reducer(state: StudentsType, action: IAction) {
+  if (action.type === 'reset') {
+    return initialState
+  }
+  const result: any = { ...state }
+  result[action.type.toLocaleLowerCase()] = action.value
+
+  return result
+}
 
 const StudentManagement = () => {
   // Loading useStates
@@ -13,6 +38,39 @@ const StudentManagement = () => {
   const [loading, setLoading] = useState(true)
   const [currentPage, setCurrentPage] = useState(1)
   const [dataPerPage] = useState(10)
+
+  // Form useStates
+  const [state, dispatch] = useReducer<Reducer<StudentsType, IAction>, StudentsType>(
+    reducer,
+    initialState,
+    () => initialState
+  )
+  const { id, name, age, course, note } = state
+
+  // Bootstrap modal useStates
+  const [show, setShow] = useState<boolean>(false)
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
+  // Actions
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault()
+    const student: StudentsType = {
+      id: id,
+      name: name,
+      age: age,
+      course: course,
+      note: note
+    }
+    await addStudent(student)
+    dispatch({ type: 'reset' })
+    populateStudentData()
+    handleClose()
+  }
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target
+    dispatch({ type: id, value })
+  }
 
   useEffect(() => {
     populateStudentData()
@@ -109,7 +167,51 @@ const StudentManagement = () => {
   return (
     <div>
       <h1 id='tableLabel'>Student management</h1>
-      <ModalStudentForm populateStudentData={populateStudentData} />
+      <div className='d-grid gap-2 d-md-flex justify-content-md-end'>
+        <Button type='button' variant='primary' className='me-1' onClick={handleShow}>
+          Register
+        </Button>
+      </div>
+      <Modal show={show} onHide={handleClose} centered>
+        <Form onSubmit={handleSubmit}>
+          <Modal.Header closeButton>
+            <Modal.Title>Student Management</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <FloatingLabel className='mb-3' controlId='Id' label='Student ID'>
+              <Form.Control type='text' placeholder='Student ID' value={id} onChange={onChange} autoFocus />
+            </FloatingLabel>
+            <FloatingLabel className='mb-3' controlId='Name' label='Student Name'>
+              <Form.Control type='text' placeholder='Student Name' value={name} onChange={onChange} />
+            </FloatingLabel>
+            <FloatingLabel className='mb-3' controlId='Age' label='Age'>
+              <Form.Control type='number' placeholder='Age' value={age > 0 ? age : ''} onChange={onChange} />
+            </FloatingLabel>
+            <FloatingLabel className='mb-3' controlId='Course' label='Course'>
+              <Form.Control type='text' placeholder='Course' value={course} onChange={onChange} />
+            </FloatingLabel>
+            <FloatingLabel controlId='Note' label='Note'>
+              <Form.Control
+                as='textarea'
+                placeholder='Note'
+                style={{
+                  height: '100px'
+                }}
+                value={note}
+                onChange={onChange}
+              />
+            </FloatingLabel>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant='secondary' type='button' onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant='primary' type='submit'>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
       {contents}
     </div>
   )
--- ClientApp/src/pages/components/ModalStudentForm.tsx
+++ ClientApp/src/pages/components/ModalStudentForm.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, Fragment, Reducer, useReducer, useState } from 'react'
+/* import { FormEvent, Fragment, Reducer, useReducer, useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import FloatingLabel from 'react-bootstrap/FloatingLabel'
 import Form from 'react-bootstrap/Form'
@@ -10,110 +10,16 @@ interface FormProps {
   populateStudentData(): Promise<void>
 }
 
-interface IAction {
-  type: string
-  value?: string
-}
-
-const initialState: StudentsType = {
-  id: '',
-  name: '',
-  age: Number(''),
-  course: '',
-  note: ''
-}
-
-function reducer(state: StudentsType, action: IAction) {
-  if (action.type === 'reset') {
-    return initialState
-  }
-  const result: any = { ...state }
-  result[action.type.toLocaleLowerCase()] = action.value
-
-  return result
-}
 
 const ModalStudentForm = (props: FormProps) => {
-  // Form useStates
-  const [state, dispatch] = useReducer<Reducer<StudentsType, IAction>, StudentsType>(
-    reducer,
-    initialState,
-    () => initialState
-  )
-  const { id, name, age, course, note } = state
 
-  // Bootstrap modal useStates
-  const [show, setShow] = useState<boolean>(false)
-  const handleClose = () => setShow(false)
-  const handleShow = () => setShow(true)
-  const handleSubmit = async (event: FormEvent) => {
-    event.preventDefault()
-    const student: StudentsType = {
-      id: id,
-      name: name,
-      age: age,
-      course: course,
-      note: note
-    }
-    await addStudent(student)
-    dispatch({ type: 'reset' })
-    props.populateStudentData()
-    handleClose()
-  }
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { id, value } = e.target
-    dispatch({ type: id, value })
-  }
 
   return (
     <Fragment>
-      <div className='d-grid gap-2 d-md-flex justify-content-md-end'>
-        <Button type='button' variant='primary' className='me-1' onClick={handleShow}>
-          Register
-        </Button>
-      </div>
-      <Modal show={show} onHide={handleClose} centered>
-        <Form onSubmit={handleSubmit}>
-          <Modal.Header closeButton>
-            <Modal.Title>Student Management</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <FloatingLabel className='mb-3' controlId='Id' label='Student ID'>
-              <Form.Control type='text' placeholder='Student ID' value={id} onChange={onChange} autoFocus />
-            </FloatingLabel>
-            <FloatingLabel className='mb-3' controlId='Name' label='Student Name'>
-              <Form.Control type='text' placeholder='Student Name' value={name} onChange={onChange} />
-            </FloatingLabel>
-            <FloatingLabel className='mb-3' controlId='Age' label='Age'>
-              <Form.Control type='number' placeholder='Age' value={age > 0 ? age : ''} onChange={onChange} />
-            </FloatingLabel>
-            <FloatingLabel className='mb-3' controlId='Course' label='Course'>
-              <Form.Control type='text' placeholder='Course' value={course} onChange={onChange} />
-            </FloatingLabel>
-            <FloatingLabel controlId='Note' label='Note'>
-              <Form.Control
-                as='textarea'
-                placeholder='Note'
-                style={{
-                  height: '100px'
-                }}
-                value={note}
-                onChange={onChange}
-              />
-            </FloatingLabel>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant='secondary' type='button' onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant='primary' type='submit'>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Form>
-      </Modal>
+      
     </Fragment>
   )
 }
 
 export default ModalStudentForm
+ */
