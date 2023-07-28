21f6ec65b752f73420c71b2c1d581fb823353df1
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-07-28 11:44:37 +0700

    Update add bootstrap icons to project

--- ClientApp/src/index.jsx
+++ ClientApp/src/index.jsx
@@ -1,3 +1,4 @@
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
