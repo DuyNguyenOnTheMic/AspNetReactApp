228bfc658d1eb69f91ececdee03b86d1f9aa04ba
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-07-31 11:35:32 +0700

    Update search grid for student management

--- ClientApp/src/pages/components/students/TableStudent.tsx
+++ ClientApp/src/pages/components/students/TableStudent.tsx
@@ -1,7 +1,8 @@
 import moment from 'moment'
 import { Fragment, useState } from 'react'
-import Button from 'react-bootstrap/esm/Button'
-import Table from 'react-bootstrap/esm/Table'
+import Button from 'react-bootstrap/Button'
+import Table from 'react-bootstrap/Table'
+import Form from 'react-bootstrap/esm/Form'
 import useSortableData from 'src/pages/hooks/useSortableData'
 import { StudentsType } from 'src/types/studentTypes'
 import CustomPagination from '../CustomPagination'
@@ -35,10 +36,35 @@ const TableStudent = ({ data, handleShow }: TableProps) => {
 
   return (
     <Fragment>
-      <div className='d-grid gap-2 d-md-flex justify-content-md-end'>
-        <Button type='button' variant='primary' className='me-1' onClick={() => handleShow('create')}>
-          <i className='bi bi-plus-circle'></i> Register
-        </Button>
+      <div className='d-flex justify-content-between align-items-center mx-2 row mt-75'>
+        <div className='col-sm-12 col-lg-4 d-flex justify-content-center justify-content-lg-start'>
+          <div className='my-2'>
+            <label className='text-nowrap'>
+              Hiển thị
+              <Form.Select className='d-inline-block mx-2 w-50'>
+                <option value='10'>10</option>
+                <option value='25'>25</option>
+                <option value='50'>50</option>
+                <option value='-1'>tất cả</option>
+              </Form.Select>
+              dữ liệu
+            </label>
+          </div>
+        </div>
+        <div className='col-sm-12 col-lg-8 ps-xl-75 px-0'>
+          <div className='d-flex align-items-center justify-content-center justify-content-lg-end flex-lg-nowrap flex-wrap'>
+            <div className='me-2'>
+              <div>
+                <Form.Control type='search' placeholder='Search...'></Form.Control>
+              </div>
+            </div>
+            <div className='d-inline-flex mt-50'>
+              <Button type='button' variant='primary' onClick={() => handleShow('create')}>
+                <i className='bi bi-plus-circle'></i> Register
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
       <Table striped hover responsive aria-labelledby='tableLabel'>
         <thead>
